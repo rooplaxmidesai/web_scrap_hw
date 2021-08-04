364966acff86e19f89ba22814c149ab05e9cbe1f
Padmanabha Desai <rooplaxmi.desai@gmail.com>
2021-08-04 06:54:15 -0500

    week22 homework

---
+++ README.md
@@ -0,0 +1,24 @@
+# Week 22 homework
+ This repository holds my week22  homework assignment.
+ 
+## Week22 Homework Assignment Question 1 and 2
+#### Q.1 Create a python function to scrape Yelp data for 50 restaurants close to you. Think about what data you want to scrape aside from the restaurant name and location. You must pick at least three other fields. You can use beautiful soup, selenium, scrapy, and/or splinter.
+#### Q.2 Create an API that calls your scrape function and stores the data when you call /scrape endpoint. The data you scrape should be viewable when you go to /all .
+ - Home page
+    * !['Screenshot1'](./Week22_screenshot_1.PNG?raw=true "Home page")
+ - A /all endpoint that displays all scraped data
+	* !['Screenshot2'](./Week22_screenshot_2.PNG?raw=true "all endpoint")
+	- Notice that this has not captured phone number, address and price range. When I tried it earlier it did capture it, below is screen shot od .csv file for that.I am not sure why sometimes the data is not captured.
+	- !['Screenshot4'](./Week22_screenshot_4.PNG?raw=true "all endpoint")
+ - A /scrape endpoint scrapes yelp restaurants 
+    * !['Screenshot3'](./Week22_screenshot_3.PNG?raw=true "scrape endpoint")
+
+## Week21 Homework Assignment Question 3
+#### Q.3 What is web scraping? Why is it helpful? Why is it sometimes in a legal grey area or just plain illegal?
+   - Web scraping is the process of gathering information/extracting data from the Internet. It usually refer to a process that involves automation. Some websites don’t like it when automatic scrapers gather their data, while others don’t mind. 
+   - Web scraping can be useful for 
+	- Price Monitoring : Web Scraping can be used by companies to scrap the price of products to compare their prices and optimizie thier prices. 
+	- Market Research : Web scraping can be helpful for companies in analyzing consumer trends and understand which direction the company should move in the future. 
+	- Sentiment Analysis : To get the idea of how thier product is doing in the market by extracting reviews and analysing the sentiment.
+	- Email Marketing : Collect Email ID’s from various sites using web scraping and then send bulk promotional and marketing Emails to all the people owning these Email ID’s.
+   - The legality of web scraping depends on the terms and conditions of the the website and what you will do with that data. If conditions say you can not use scrapers and still doing so can result in ban in using that website. Search engines like google and bing scrape the website to bring those search results and that in turn brings traffic to thier website. if web scrapres acces the sites that require authentication and based on that provide secure data which is captured in we scraping then release it to outside world this could be illegal. The illegal action here is not that you scraped the content you have already access to but shaing it with the rest of the world. This could be comparable to buying a movie on DVD/Blueray, ripping it on your PC and uploading a pirated version for everyone to see.
---
+++ web_scraping_hw.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+#import necessary libraries
+#pip install flask
+from flask import Flask, json,render_template,redirect,url_for
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+app  = Flask(__name__)
+
+
+#decorator
+@app.route("/")
+def echo_hello():
+    return_text = """<p>Web Scraping 50 restaurant near me from yelp!</p>
+                     <p>/scrape endpoint will call the function to scrape yelp and store data in CSV file</p>
+                     <p>/all endpoint will display scrapped data</p>
+                      """
+    return return_text
+
+@app.route("/scrape")
+def web_scrape():
+    i=0
+    restaurant_list = []
+    #each page has 10 restaurants. To get 50 restaurants loop thourgh 5 times
+    while i<=40:
+        yurl = f'https://www.yelp.com/search?find_desc=Restaurants&find_loc=Ballwin%2C+MO+63021&start={i}'
+        response = requests.get(yurl)
+        print(yurl)
+        page_soup = bs(response.text, 'html.parser')
+        
+        #Get all the restaurant names in each page. One page has 10 restaurants
+        business = page_soup.find_all("div",class_="container__09f24__sxa9- hoverable__09f24__3HkY2 margin-t3__09f24__hHZsm margin-b3__09f24__3h89A padding-t3__09f24__1VTAn padding-r3__09f24__11Xv2 padding-b3__09f24__2I83c padding-l3__09f24__1JEx9 border--top__09f24__37VAs border--right__09f24__Z9jGU border--bottom__09f24__3lElq border--left__09f24__akfOa border-color--default__09f24__3Epto")
+        try:
+            for b in business:
+                #Restaurant Name
+                rest_name = b.find("div",class_="businessName__09f24__3Ml2X display--inline-block__09f24__3SvIn border-color--default__09f24__3Epto").find("a").text
+                
+                #Price range
+                price = b.find("span",class_="priceRange__09f24__2GspP css-xtpg8e")
+                if price!=None:
+                    price = price.text
+                else:
+                    price = "Not Specified"
+                
+                #Facilities like - takeout, dinein, delivery etc
+                #facility = b.find_all("span",class_="raw__09f24__3Azrj")
+                #f_list=[]
+                #if facility!=None:
+                #    for f in facility:
+                #        f_list.append(f.text)
+                #else:
+                #    f_list.append("Not Specified")
+                
+                #Facilities like - takeout, dinein, delivery etc
+                facility =  b.find_all("ul",class_="undefined list__09f24__36n6_") 
+                #print(len(facility))
+                f_list=[]
+                if facility!=None:
+                    for f in facility:
+                        f_list.append(f.find("span",class_="raw__09f24__3Azrj").text)
+                else:
+                    f_list.append("Not Specified")
+                
+                #First review
+                first_review = b.find("p",class_="css-e81eai").text
+                
+                #rating
+                ratings = b.find("span", class_="display--inline__09f24__nqZ_W border-color--default__09f24__3Epto").div.get('aria-label')
+
+                #Review Count
+                review_count = b.find("span",class_="reviewCount__09f24__3GsGY css-e81eai")
+                if review_count!=None:
+                    review_count = review_count.text
+                else:
+                    review_count = "Not specified"
+        
+            
+                #Get list of food category
+                food_category = b.find_all("p",class_="css-1j7sdmt")
+                #print(len(food_category))
+                fc_list=[]
+                if food_category!=None:
+                    for fc in food_category:
+                        #Find text for each food category
+                        fcat = fc.find_all("p",class_="text__09f24__2NHRu css-1hx6l2b")
+                        if(fcat!=None):
+                            for c in fcat:
+                                if(c!=None):
+                                    fc_list.append(c.text)
+                                else:
+                                    fc_list.append("Not Specified")
+                        else:
+                            fc_list.append("Not Specified")
+                else:
+                    fc_list.append("Not Specified")
+                
+                
+                #Phone number and address:
+                address = b.find_all("p" ,class_="css-8jxw1i")
+                cnt = 0
+                phone_number="Not Specified"
+                addr="Not Specified"
+                if address!=None:
+                    for a in address:
+                        if cnt==0:
+                            phone_number = a.text
+                        if cnt==1:
+                            addr = a.find("span",class_="raw__09f24__3Azrj").text
+                        cnt = cnt + 1
+                    #print("Phone and address",phone_number,addr)
+            
+                #Dictionary of restaurant name, rating etc.    
+                rest_dict = {"Name":rest_name,
+                            "Phone Number":phone_number,
+                            "Address":addr,
+                            "Price range":price,
+                            "Facility":f_list,
+                            "1st Review": first_review,
+                            "Ratings":ratings,
+                            "Review Count":review_count,
+                            "Food Category":fc_list
+                            }
+
+                restaurant_list.append(rest_dict)
+        except AttributeError as e:
+                print(e)
+            
+        #Go to next 10 restaurants
+        i= i + 10
+        
+    rest_df = pd.DataFrame(restaurant_list)
+    rest_df.to_csv("./static/yelp_ballwin_rest.csv")
+    #return render_template('index.html',  tables=[rest_df.to_html(classes='data', header="true")])
+    #render template is always looking in tempate folder
+    #return "<p>Web Scraping 50 restaurants near me from yelp and storing them in CSV file!</p>"
+    return redirect(url_for("show_data"))
+
+
+@app.route("/all")
+def show_data():
+    rest_df = pd.read_csv("./static/yelp_ballwin_rest.csv")
+    #render template is always looking in tempate folder
+    return render_template('index.html',  tables=[rest_df.to_html(classes='data', header="true")])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
